beb8c0db74cbacd146ffad47d0e659393fae61f5
marcellinuselbert <elbertmarcellinus@gmail.com>
2023-12-04 02:25:44 +0700

    Add animation on detail page

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -28,13 +28,15 @@ export default function DocumentDetail() {
 
   return (
     <div className="p-20">
-      <h1 className="text-center text-3xl font-bold text-blue-500">
+      <h1 className="text-center text-3xl font-bold text-blue-500 animate-fade-in-fwd-3">
         {document?.title}
       </h1>
 
-      <p className="text-center text-blue-400 mt-1">{document?.path}</p>
+      <p className="text-center text-blue-400 mt-1 animate-fade-in-fwd-3">
+        {document?.path}
+      </p>
 
-      <p className="text-center text-lg mt-10 text-gray-800 font-medium">
+      <p className="text-center text-lg mt-10 text-gray-800 font-medium animate-fade-in-fwd-3">
         {document?.text}
       </p>
     </div>
